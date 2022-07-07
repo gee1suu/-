971c4ed6343dbe082d688974199b740294066168
안지수 <80879666+gee1suu@users.noreply.github.com>
2022-07-07 10:53:07 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int idx = 0; idx < N; idx++) {
+			arr[idx] = Integer.parseInt(st.nextToken());
+		}
+
+		int M = 0;
+		for (int idx = 0; idx < N; idx++) {
+			if (arr[idx] > M)
+				M = arr[idx];
+		}
+
+		double avg = 0;
+		for (int idx = 0; idx < N; idx++) {
+			avg += arr[idx] / (double) M * 100;
+		}
+		avg /= N;
+		bw.write(String.valueOf(avg));
+		bw.flush();
+
+	}
+
+}
