f2ab42d3463935fb96f9c340c83dfe4ba8ca8028
안지수 <80879666+gee1suu@users.noreply.github.com>
2022-07-12 15:44:54 +0900

    [Silver III] Title: 소수 구하기, Time: 312 ms, Memory: 23040 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,27 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		int m = scanner.nextInt();
-		int n = scanner.nextInt();
-		
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+
 		boolean[] prime = new boolean[n + 1];
 		prime[0] = prime[1] = true;
-		
-		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-			if(prime[i]) continue;
-			for(int j = i * i; j < prime.length; j += i) {
+
+		for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if (prime[i])
+				continue;
+			for (int j = i * i; j < prime.length; j += i) {
 				prime[j] = true;
 			}
 		}
-		
-		for(int i = m; i <= n; i++) {
-			if(!prime[i]) System.out.println(i);
+
+		for (int i = m; i <= n; i++) {
+			if (!prime[i])
+				bw.write(i + "\n");
 		}
-		
-		scanner.close();
+
+		bw.flush();
 	}
 }
