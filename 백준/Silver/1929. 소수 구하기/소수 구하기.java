ecbe7e24afa51e5b3553ba7e79a40b0b58e6df01
안지수 <80879666+gee1suu@users.noreply.github.com>
2022-07-12 15:55:53 +0900

    Update 소수 구하기.java

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -18,9 +18,24 @@ public class Main {
 		boolean[] prime = new boolean[n + 1];
 		prime[0] = prime[1] = true;
 
+		// 예시(n = 9)
+		// 9 = 1 * 9
+		// 9 = 2 * 4.xx
+		// 9 = 3 * 3
+		// 9 = 4 * 2.xx
+		// 9 = 5 * 1.xx
+		// 9 = 6 * 1.xx
+		// 9 = 7 * 1.xx
+		// 9 = 8 * 1.xx
+		// 9 = 9 * 1
+		
+		// 3(9의 제곱근) 이후로 나눠진 몫은 1 이하 3 미만의 수이므로 계산해 볼 필요 X
+		// 따라서 i의 범위는 i <= Math.sqrt(prime.length)
 		for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+			// i가 소수가 아니면 지나감
 			if (prime[i])
 				continue;
+			// i가 소수이면 i를 제외한 i의 배수가 소수가 아님을 표시
 			for (int j = i * i; j < prime.length; j += i) {
 				prime[j] = true;
 			}
